3cb8a0888987761b9bfb2adf560682137d93d3fb
monocvet <monocvet@mail.ru>
2024-05-01 02:25:41 +0300

    DZ-16 Использование метода Lock

--- src/main/java/org/example/HomeWork16/Main.java
+++ src/main/java/org/example/HomeWork16/Main.java
@@ -1,16 +1,19 @@
 package org.example.HomeWork16;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Main {
     public static void main(String[] args) {
         StringBuilder stringBuilder = new StringBuilder("a");
+        Lock lock = new ReentrantLock();
 
-        ThreadExample thread1 = new ThreadExample(stringBuilder);
-        ThreadExample thread2 = new ThreadExample(stringBuilder);
-        ThreadExample thread3 = new ThreadExample(stringBuilder);
+        ThreadExample thread1 = new ThreadExample(stringBuilder, lock);
+        ThreadExample thread2 = new ThreadExample(stringBuilder, lock);
+        ThreadExample thread3 = new ThreadExample(stringBuilder, lock);
 
         thread1.start();
         thread2.start();
         thread3.start();
-
     }
 }
--- src/main/java/org/example/HomeWork16/ThreadExample.java
+++ src/main/java/org/example/HomeWork16/ThreadExample.java
@@ -1,22 +1,26 @@
 package org.example.HomeWork16;
 
-public class ThreadExample extends Thread{
-    private StringBuilder sb;
+import java.util.concurrent.locks.Lock;
 
-    public ThreadExample(StringBuilder sb) {
+public class ThreadExample extends Thread {
+    private StringBuilder sb;
+    private Lock lock;
+
+    public ThreadExample(StringBuilder sb, Lock lock) {
         this.sb = sb;
+        this.lock = lock;
     }
 
     @Override
     public void run() {
-        synchronized (sb) {
-            for (int i = 0; i < 100; i++) {
-                System.out.print(sb);
-            }
-            System.out.println();
-            char symbol = sb.charAt(0);
-            sb.setCharAt(0, ++symbol);
+        lock.lock();
+        for (int i = 0; i < 100; i++) {
+            System.out.print(sb);
         }
-
+        System.out.println();
+        char symbol = sb.charAt(0);
+        sb.setCharAt(0, ++symbol);
+        lock.unlock();
     }
 }
+
